88140897d4e7f84cd573aefaae0a52a6bc8d7f61
AHMED MOAHMED <aabdalnaby073@gmail.com>
2022-09-16 23:31:56 +0200

    payment app in c s

---
+++ terminalheader.h
@@ -0,0 +1,10 @@
+#ifndef TERMINALHEADER_H_INCLUDED
+#define TERMINALHEADER_H_INCLUDED
+struct terminal_data
+{
+   double transAmount;
+    double maxTransAmount;
+    unsigned char transactionDate[11];
+};
+
+#endif
